3d137d7734dd3d3e3423ed1e6a680b215743af0e
Rafael Forbeck <rafaelforbeck@gmail.com>
2024-05-07 20:23:54 -0300

    Adiciona barra de vida

---
+++ Assets/MyAssets/Scripts/Life.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Life : MonoBehaviour
+{
+    public Image barImage;
+    public float maxLife;
+    
+    private float currentLife;
+
+    private void Start()
+    {
+        currentLife = maxLife;
+        UpdateBar();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentLife -= damage;
+        currentLife = Mathf.Clamp(currentLife, 0, maxLife);
+        UpdateBar();
+        if (currentLife == 0)
+        {
+            // Player morreu
+        }
+    }
+
+    public void HealingHealth(float healing)
+    {
+        currentLife += healing;
+        currentLife = Mathf.Clamp(currentLife, 0, maxLife);
+        UpdateBar();
+    }
+         
+    void UpdateBar()
+    {
+        barImage.fillAmount = currentLife / maxLife;
+    }
+}
